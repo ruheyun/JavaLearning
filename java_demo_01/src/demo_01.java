97f5746c6d5151b0bb68a97ca3fe7d7fb85cb010
ruheyun <1654033661@qq.com>
2024-01-02 22:09:42 +0800

    update demo 01

---
+++ java_demo_01/src/demo_01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class demo_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个三位整数：");
+        int abc = sc.nextInt();
+        int c = abc % 10;
+        int b = abc / 10 % 10;
+        int a = abc / 100 % 10;
+        System.out.println("个位" + c + "    十位" + b + "    百位" + a);
+    }
+}
